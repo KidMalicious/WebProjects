701ca702ec7b91fdb5f205dd360cee1b6cf1aac4
KidMalicious <akf.behopi@gmail.com>
2020-10-23 23:39:11 -0700

    added registerUser that encrypts password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,39 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+//@desc     Register new user
+//@route    POST /api/users
+//@access   Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExist = await User.findOne({ email })
+
+    if (userExist) {
+        res.status(400)
+        throw new Error('User already exist')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 //@desc     GET user profile
 //@route    GET /api/users/profile
 //@access   private
@@ -34,7 +67,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
-            isAdmin: user.isAdmin, 
+            isAdmin: user.isAdmin,
         })
     } else {
         res.status(401)
@@ -42,4 +75,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+
+
+export { authUser, getUserProfile, registerUser}
