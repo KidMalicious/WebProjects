1d09e6b82da0a374bf6b403c809fa5941ed87329
KidMalicious <akf.behopi@gmail.com>
2020-10-23 23:11:26 -0700

    added userController to authorize and authenticate user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler"
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 
 //@desc     Auth user & get token
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
@@ -23,4 +24,22 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser }
+//@desc     GET user profile
+//@route    GET /api/users/profile
+//@access   private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin, 
+        })
+    } else {
+        res.status(401)
+        throw new Error('User not found')
+    }
+})
+
+export { authUser, getUserProfile }
