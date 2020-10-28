75c7083e507eb29320cf15ec5cb6f61e979e20a6
KidMalicious <akf.behopi@gmail.com>
2020-10-27 17:36:03 -0700

    Added Shipping Screen, Payment Screen, Place Order Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreens'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
   return (
@@ -16,9 +19,12 @@ const App = () => {
       <Header />
       <main className="py-4">
         <Container>
-          <Route path='/login' component={LoginScreen}  />
-          <Route path='/register' component={RegisterScreen}  />
-          <Route path='/profile' component={ProfileScreen}  />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer} from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from './reducers/userReducers'
+import { orderCreateReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -12,7 +13,8 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer
+    userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer
 })
 
 localStorage.clear()
@@ -20,11 +22,13 @@ localStorage.clear()
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAdress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const initialState = {
-    cart: {
-        cartItems: cartItemsFromStorage
-    },
-    userLogin: {userInfo: userInfoFromStorage}
+    cart: {cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage},
+    userLogin: {userInfo: userInfoFromStorage},
+
 }
 
 const middleware = [thunk]
